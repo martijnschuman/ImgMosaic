bf8567234813e2d29664fef30a154318e38f1f23
Martijn Schuman <martijnschuman@hotmail.com>
2025-07-17 17:41:13 +0200

    Cleanup

---
+++ ImgMosaic/Models/ImgMosaicGenerator.cs
@@ -0,0 +1,35 @@
+﻿using OpenCvSharp;
+
+namespace ImgMosaic.Models;
+
+public class ImgMosaicGenerator {
+    public List<Image> loadImages(string folderPath) {
+        List<Image> images = new List<Image>();
+
+        if (Directory.Exists(folderPath)) {
+            foreach (var imagePath in Directory.EnumerateFiles(folderPath, "*.png")) {
+                using var tileImage = Cv2.ImRead(imagePath, ImreadModes.Color);
+                if (tileImage.Empty()) {
+                    Console.WriteLine($"Failed to load image: {imagePath}");
+                    continue;
+                }
+
+                var img = new Image(
+                    filePath: imagePath,
+                    fileName: Path.GetFileNameWithoutExtension(imagePath),
+                    fileExtension: Path.GetExtension(imagePath),
+                    cols: tileImage.Width,
+                    rows: tileImage.Height,
+                    sizeInBytes: tileImage.Total() * tileImage.ElemSize(),
+                    channels: tileImage.Channels()
+                );
+                images.Add(img);
+            }
+        }
+        else {
+            throw new Exception("Folder not found: " + folderPath);
+        }
+
+        return images;
+    }
+}
--- ImgMosaic/Program.cs
+++ ImgMosaic/Program.cs
@@ -1,31 +1,13 @@
-﻿using System;
-using System.IO;
-using OpenCvSharp;
-
-string inputPath = Path.Combine(Directory.GetCurrentDirectory(), "input");
-string outputPath = Path.Combine(Directory.GetCurrentDirectory(), "output");
-string targetPath = Path.Combine(Directory.GetCurrentDirectory(), "target");
+﻿using ImgMosaic.Models;
 
 
+string inputPath = Path.Combine(Directory.GetCurrentDirectory(), "src", "input");
+string outputPath = Path.Combine(Directory.GetCurrentDirectory(), "src", "output");
+string targetPath = Path.Combine(Directory.GetCurrentDirectory(), "src", "target");
 
+ImgMosaicGenerator mosaic = new ImgMosaicGenerator();
 
+List<Image> inputImages = mosaic.loadImages(inputPath);
+List<Image> outputImages = mosaic.loadImages(outputPath);
+List<Image> targetImage = mosaic.loadImages(targetPath);
 
-if (Directory.Exists(inputPath)) {
-    string[] imageFiles = Directory.GetFiles(inputPath, "*.png");
-
-    foreach (string imagePath in imageFiles) {
-        Mat tileImage = Cv2.ImRead(imagePath, ImreadModes.Color);
-
-        if (tileImage.Empty()) {
-            Console.WriteLine($"Failed to load image: {imagePath}");
-        }
-        else {
-            Console.WriteLine($"Loaded tile: {Path.GetFileName(imagePath)}, Size: {tileImage.Width}x{tileImage.Height}");
-            string fileName = Path.GetFileNameWithoutExtension(imagePath);
-
-        }
-    }
-}
-else {
-    Console.WriteLine("Folder not found: " + inputPath);
-}
